33ef0f62ea1d29fa7da282e6a80da121d425814a
AVENATEC-LDA <dev.department@avenatec.it.com>
2026-06-24 11:27:10 +0100

    Enforce TLS for dynamic SMTP mailer

--- packages/Webkul/Core/src/Mail/Transport/DynamicSmtpTransport.php
+++ packages/Webkul/Core/src/Mail/Transport/DynamicSmtpTransport.php
@@ -2,6 +2,7 @@
 
 namespace Webkul\Core\Mail\Transport;
 
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\Mailer\SentMessage;
 use Symfony\Component\Mailer\Transport\AbstractTransport;
 use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
@@ -15,7 +16,19 @@ class DynamicSmtpTransport extends AbstractTransport
     {
         $transport = $this->buildTransport();
 
-        $transport->send($message->getOriginalMessage(), $message->getEnvelope());
+        try {
+            $transport->send($message->getOriginalMessage(), $message->getEnvelope());
+        } catch (\Throwable $e) {
+            Log::error('Dynamic SMTP mail delivery failed.', [
+                'host'       => $this->value('emails.configure.smtp.host', config('mail.mailers.smtp.host')),
+                'port'       => $this->value('emails.configure.smtp.port', config('mail.mailers.smtp.port')),
+                'encryption' => $this->value('emails.configure.smtp.encryption', config('mail.mailers.smtp.encryption')),
+                'username'   => $this->mask((string) $this->value('emails.configure.smtp.username', config('mail.mailers.smtp.username'))),
+                'error'      => $e->getMessage(),
+            ]);
+
+            throw $e;
+        }
     }
 
     /**
@@ -42,6 +55,14 @@ class DynamicSmtpTransport extends AbstractTransport
             tls: strtolower((string) $encryption) === 'ssl',
         );
 
+        $transport->setAutoTls(strtolower((string) $encryption) === 'tls');
+
+        $transport->getStream()->setStreamOptions([
+            'ssl' => [
+                'crypto_method' => $this->cryptoMethod(),
+            ],
+        ]);
+
         $transport->setUsername((string) $username);
 
         $transport->setPassword((string) $password);
@@ -57,6 +78,32 @@ class DynamicSmtpTransport extends AbstractTransport
         $value = core()->getConfigData($key);
 
         return $value !== null && $value !== '' ? $value : $fallback;
+    }
+
+    /**
+     * ZeptoMail requires TLS 1.2 or newer.
+     */
+    protected function cryptoMethod(): int
+    {
+        $method = STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT;
+
+        if (defined('STREAM_CRYPTO_METHOD_TLSv1_3_CLIENT')) {
+            $method |= STREAM_CRYPTO_METHOD_TLSv1_3_CLIENT;
+        }
+
+        return $method;
+    }
+
+    /**
+     * Mask sensitive log context.
+     */
+    protected function mask(string $value): string
+    {
+        if ($value === '') {
+            return '';
+        }
+
+        return substr($value, 0, 4).str_repeat('*', max(strlen($value) - 8, 0)).substr($value, -4);
     }
 
     /**

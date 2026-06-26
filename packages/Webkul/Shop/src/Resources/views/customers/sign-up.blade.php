ddf104f8c2254c75954ecee95c24b35aa062e448
AVENATEC-LDA <dev.department@avenatec.it.com>
2026-06-26 15:35:42 +0100

    Update footer copyright text on shop pages

--- packages/Webkul/Shop/src/Resources/views/customers/sign-up.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/sign-up.blade.php
@@ -273,7 +273,7 @@
 		</div>
 
         <p class="mb-4 mt-8 text-center text-xs text-zinc-500">
-            @lang('shop::app.customers.signup-form.footer', ['current_year'=> date('Y') ])
+            COPYRIGHT © {{ date('Y') }} - BERN OKULEKA. CRIADO E DESENVOLVIDO PELA <a href="https://avenatec.it.com" target="_blank" rel="noopener noreferrer">AVENATEC</a>
         </p>
 	</div>
 
